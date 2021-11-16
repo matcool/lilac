b5bfc76d29126c923d7e96ea62e73a6c76094a68
HJfod <60038575+HJfod@users.noreply.github.com>
2021-11-16 22:24:28 +0200

    some src

--- api/lilac/Hook.hpp
+++ api/lilac/Hook.hpp
@@ -6,20 +6,20 @@
 namespace lilac {
     using address_t = uintptr_t;
 
+    class Mod;
+
     class LILAC_DLL Hook {
         private:
+            Mod*  m_owner;
             void* m_address;
             void* m_detour;
             void* m_handle = nullptr;
-            bool m_enabled;
+            bool  m_enabled;
 
         public:
-            inline address_t getAddress() const {
-                return reinterpret_cast<address_t>(m_address);
-            }
-            inline bool isEnabled() const {
-                return m_enabled;
-            }
+            address_t getAddress() const { return m_address; }
+            bool isEnabled() const { return m_enabled; }
+            Mod* getOwner() const { return m_owner; }
 
             Hook(address_t addr) :
                 m_address(reinterpret_cast<void*>(addr)), m_enabled(false) {}
