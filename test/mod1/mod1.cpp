82e0b7920a93f8188d5eea1c8a966bfad865504b
HJfod <60038575+HJfod@users.noreply.github.com>
2021-12-08 21:54:49 +0200

    create zip CMakeLists

--- test/mod1/mod1.cpp
+++ test/mod1/mod1.cpp
@@ -1,20 +1,5 @@
 #include "mod1.hpp"
 
-#pragma section(".lilac", read)
-
-__declspec(allocate(".lilac"))
-static const char* g_modinfo =
-R"JSON({
-	"version":      "v1.0.0",
-	"id":           "com.lilac.test_one",
-    "name":         "Lilac Test 1",
-    "developer":    "Lilac Team",
-    "description":  "Test Mod #1",
-    "details":      "Test Mod #1 for making sure hooking, loading and sharing interfaces works.",
-    "credits":      null,
-	"dependencies": null
-})JSON";
-
 void TestMod1::setup() {
     this->logMessage("Hi from TestMod1");
 }
